6414293700dff8c0a7d86536d472ebb79dc604bf
Vladyslava <vladyslava@hkhk.edu.ee>
2026-06-08 15:45:39 +0300

    Delete autorent_project directory

--- autorent_project/admin/logout.php
+++
@@ -1,4 +0,0 @@
-<?php
-require_once __DIR__ . '/../inc/functions.php';
-unset($_SESSION['is_admin']);
-redirect('login.php');
--- autorent_project/public/logout.php
+++
@@ -1,4 +0,0 @@
-<?php
-require_once __DIR__ . '/../inc/functions.php';
-session_destroy();
-redirect('index.php');
