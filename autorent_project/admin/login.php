7cde415134f6305768ff6c9ccb2b542b0e65cbd5
Vladyslava <vladyslava@hkhk.edu.ee>
2026-06-10 14:46:54 +0300

    Update login.php

--- autorent_project/admin/login.php
+++ autorent_project/admin/login.php
@@ -8,7 +8,7 @@ $passwordAdmin = 'admin123';
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
-    $email = trim($_POST['email']);
+    $email = $_POST['email'];
     $password = $_POST['password'];
 
     if ($email == $emailAdmin && $password == $passwordAdmin) {
